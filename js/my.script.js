374c77b0106643ec0856b22103b58fac4fc0d80f
moebeck-coder <69625167+moebeck-coder@users.noreply.github.com>
2020-08-14 18:46:03 +0900

    Update my.script.js

--- js/my.script.js
+++ js/my.script.js
@@ -1,50 +1,5 @@
-
-  // pc用トップ
-    // var images = [ 'url(img/photograph-2015-06_pc.JPG)', 'url(img/photograph-2017-01-02_pc.jpg)', 'url(img/photograph-2017-02-04_pc.JPG)' ];
-    // $(function(){
-    // var backgroundRandom = function(){
-    //   var number = Math.floor(Math.random() * images.length);
-    //   var selectedImg = images[number];
-    //   $('#randomImg-pc').css('background-image',selectedImg);
-    // };
-    // setInterval(backgroundRandom, 5000);
-    // });
-  
-    // モバイル用トップ
-      // var images = [ 'url(img/randomImg-01_sp.JPG)', 'url(img/randomImg-02_sp.JPG)', 'url(img/randomImg-03_sp.JPG)' ];
-      // $(function(){
-      // var backgroundRandom = function(){
-      //   var number = Math.floor(Math.random() * images.length);
-      //   var selectedImg = images[number];
-      //   $('#randomImg-sp').css('background-image',selectedImg);
-      // };
-      // setInterval(backgroundRandom, 5000);
-      // });
-
 document.addEventListener('DOMContentLoaded', function(){
-  //背景
-  if (window.matchMedia( "(max-width: 740px)" ).matches) {
-    var images = [ 'url(randomImg-01_sp.JPG)', 'url(randomImg-02_sp.JPG)', 'url(randomImg-03_sp.JPG)' ];
-  $(function(){
-  var backgroundRandom = function(){
-    var number = Math.floor(Math.random() * images.length);
-    var selectedImg = images[number];
-    $('#randomImg-sp').css('background-image',selectedImg);
-  };
-  setInterval(backgroundRandom, 5000);
-  });
-  } else {
-    var images = [ 'url(photograph-2015-06_pc.JPG)', 'url(photograph-2017-01-02_pc.jpg)', 'url(photograph-2017-02-04_pc.JPG)' ];
-    $(function(){
-    var backgroundRandom = function(){
-      var number = Math.floor(Math.random() * images.length);
-      var selectedImg = images[number];
-      $('#randomImg-pc').css('background-image',selectedImg);
-    };
-    setInterval(backgroundRandom, 5000);
-    });
-  }
-  // ハンバーガーメニュー
+   //ハンバーガーメニュー
   document.getElementById("menuButton").addEventListener("click", function(){
     this.classList.toggle("active");
     document.getElementById("spHeader-menu").classList.toggle("active");
@@ -54,11 +9,3 @@ document.addEventListener('DOMContentLoaded', function(){
 
   })
 });
-  
-
-
-
-
-
-
-      
